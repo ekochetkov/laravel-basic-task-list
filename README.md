af78fda4c05d6350fdbed1a2553e31c8d7b4a864
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2020-09-09 00:23:47 +0300

    Add task validation Add task add code

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Common
 
 `php artisan serve`
+`php artisan config:clear`
+`php artisan cache:clear`
+`php artisan route:clear`
+`php artisan view:clear`
+`php artisan optimize`
 
 # Installing laravel
 
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,7 +6,7 @@
 
     <div class="panel-body">
         <!-- Display Validation Errors -->
-        <!--include('common.errors')-->
+        @include('common.errors')
 
         <!-- New Task Form -->
         <form action="/task" method="POST" class="form-horizontal">
